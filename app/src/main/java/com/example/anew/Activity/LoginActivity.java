1fa800aef00ceaf2459856de1a4b7f355d454701
Phong Nguyen <44559337+ntp050698@users.noreply.github.com>
2019-10-21 15:47:34 +0700

    â

--- app/src/main/java/com/example/anew/Activity/LoginActivity.java
+++ app/src/main/java/com/example/anew/Activity/LoginActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,8 +27,8 @@ import retrofit2.Response;
 public class LoginActivity extends AppCompatActivity {
 
     private Button mBtnLogin;
-    private TextInputEditText mEdtUser;
-    private TextInputEditText mEdtPass;
+    private EditText mEdtUser;
+    private EditText mEdtPass;
     private List<Login> logins;
 
     String email;
